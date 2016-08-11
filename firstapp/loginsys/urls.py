80daec97c4be621f203fe0fde1b019adc0b11064
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-12 00:17:10 +0300

    Урок 11. Регистрация

--- firstapp/loginsys/urls.py
+++ firstapp/loginsys/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from loginsys         import views
 
 urlpatterns = [
-		url (r'^login/',  views.login),
-		url (r'^logout/', views.logout),
+		url (r'^login/$',    views.login),
+		url (r'^logout/$',   views.logout),
+		url (r'^register/$', views.register),
 ]
